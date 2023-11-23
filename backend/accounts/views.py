0ec3c4f1771547a7e8b532d957c634432bd28fe4
Alphius <alphiuswambua@gmail.com>
2023-11-23 16:22:32 +0300

    added register api functionality

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from .serializers import RegisterSerializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
 class LogoutView(APIView):
      permission_classes = (IsAuthenticated,)
      def post(self, request):
